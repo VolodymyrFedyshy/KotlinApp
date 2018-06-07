cc5bca4a278018a1bb2d6d236aa18f9d8ce115d4
volodymyr.fedyshyn <fedyshyn1994@gmail.com>
2018-06-07 17:13:32 +0300

    Initial commit

---
+++ app/src/main/java/com/app/lotlin/kotlinapp/CustomAdapter.kt
@@ -0,0 +1,35 @@
+package com.app.lotlin.kotlinapp
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+
+class CustomAdapter(val userList: ArrayList<User>, val context: Context): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder?.txtName?.text = userList[position].name
+        holder?.txtTitle?.text = userList[position].title
+        holder?.container.setOnClickListener({
+           // Toast.makeText(context, position.toString(), ).s
+        })
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.list_row, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val txtName = itemView.findViewById<TextView>(R.id.txtName)
+        val txtTitle = itemView.findViewById<TextView>(R.id.txtTitle)
+        val container = itemView.findViewById<TextView>(R.id.container)
+    }
+}
---
+++ app/src/main/java/com/app/lotlin/kotlinapp/User.kt
@@ -0,0 +1,3 @@
+package com.app.lotlin.kotlinapp
+
+data class User(val name: String, val title: String)
